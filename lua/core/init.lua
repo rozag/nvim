cdb42e2520192fdad972d54308515ddd08cb98e4
Alexey Mileev <alemileev@gmail.com>
2023-05-14 18:47:41 +0400

    Initial core config seems to be ready

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,7 +2,26 @@
 -- Keybindings
 --------------------------------------------------------------------------------
 
--- TODO
+-- Set <Space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Keymaps for better default experience
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set(
+  'n',
+  'k',
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  'n',
+  'j',
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true }
+)
 
 --------------------------------------------------------------------------------
 -- General options
@@ -14,93 +33,88 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
--- Set <Space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- TODO: set some options from kickstart.nvim and NvChad/lua/core/init.lua
-
 -- Highlight on search; <C-l> clears highlight
-vim.o.hlsearch = true
+vim.opt.hlsearch = true
 
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- -- Enable mouse mode
--- vim.o.mouse = 'a'
+-- Enable mouse mode
+vim.opt.mouse = "a"
 
--- -- Sync clipboard between OS and Neovim.
--- --  Remove this option if you want your OS clipboard to remain independent.
--- --  See `:help 'clipboard'`
--- vim.o.clipboard = 'unnamedplus'
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = "unnamedplus"
 
--- -- Enable break indent
--- vim.o.breakindent = true
+-- Enable break indent: every wrapped line will continue visually indented (same
+-- amount of space as the beginning of that line), thus preserving horizontal
+-- blocks of text.
+vim.opt.breakindent = true
 
--- -- Save undo history
--- vim.o.undofile = true
+-- Save undo history
+vim.opt.undofile = true
 
--- -- Case insensitive searching UNLESS /C or capital in search
--- vim.o.ignorecase = true
--- vim.o.smartcase = true
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
--- -- Keep signcolumn on by default
--- vim.wo.signcolumn = 'yes'
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
 
--- -- Decrease update time
--- vim.o.updatetime = 250
--- vim.o.timeout = true
--- vim.o.timeoutlen = 300
+-- Decrease interval for writing swap file to disk, also used by gitsigns
+vim.opt.updatetime = 250
 
--- -- Set completeopt to have a better completion experience
--- vim.o.completeopt = 'menuone,noselect'
+-- This option and "timeoutlen" determine the behavior when part of a mapped key
+-- sequence has been received. For example, if <C-f> is pressed and "timeout" is
+-- set, Nvim will wait "timeoutlen" milliseconds for any key that can follow
+-- <C-f> in a mapping.
+vim.opt.timeout = true
+vim.opt.timeoutlen = 400
 
--- -- NOTE: You should make sure your terminal supports this
--- vim.o.termguicolors = true
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
 
--- -- [[ Basic Keymaps ]]
+-- Enables 24-bit RGB color in the TUI
+vim.opt.termguicolors = true
 
--- -- Keymaps for better default experience
--- -- See `:help vim.keymap.set()`
--- vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- Global statusline. The value of this option influences when the last window
+-- will have a status line, 3 -> always and ONLY the last window
+vim.opt.laststatus = 3
 
--- -- Remap for dealing with word wrap
--- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- If in Insert, Replace or Visual mode put a message on the last line.
+vim.opt.showmode = false
 
-------------------------------------------------------- FROM NV CHAD
--- opt.laststatus = 3 -- global statusline
--- opt.showmode = false
+-- Highlight the text line of the cursor with
+vim.opt.cursorline = true
 
--- opt.clipboard = "unnamedplus"
--- opt.cursorline = true
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
--- -- Indenting
--- opt.expandtab = true
--- opt.shiftwidth = 2
--- opt.smartindent = true
--- opt.tabstop = 2
--- opt.softtabstop = 2
+-- Characters to fill the statuslines, vertical separators and special lines in
+-- the window
+vim.opt.fillchars = { eob = " " }
 
--- opt.fillchars = { eob = " " }
--- opt.ignorecase = true
--- opt.smartcase = true
--- opt.mouse = "a"
+-- This option helps to avoid all the |hit-enter| prompts caused by file
+-- messages, for example  with CTRL-G, and to avoid some other messages.
+-- s:
+-- don't give "search hit BOTTOM, continuing at TOP" or "search hit TOP,
+-- continuing at BOTTOM" messages; when using the search count do not show "W"
+-- after the count message (see S below).
+-- I:
+-- don't give the intro message when starting Vim.
+vim.opt.shortmess:append("sI")
 
--- -- disable nvim intro
--- opt.shortmess:append "sI"
+-- When on, splitting a window will put the new window below the current one.
+vim.opt.splitbelow = true
 
--- opt.signcolumn = "yes"
--- opt.splitbelow = true
--- opt.splitright = true
--- opt.termguicolors = true
--- opt.timeoutlen = 400
--- opt.undofile = true
+-- When on, splitting a window will put the new window right of the current one.
+vim.opt.splitright = true
 
--- -- interval for writing swap file to disk, also used by gitsigns
--- opt.updatetime = 250
-
--- -- go to previous/next line with h,l,left arrow and right arrow
--- -- when cursor reaches end/beginning of line
--- opt.whichwrap:append "<>[]hl"
+-- Go to previous/next line with h,l,left arrow and right arrow when cursor
+-- reaches end/beginning of line.
+vim.opt.whichwrap:append "<>[]hl"
