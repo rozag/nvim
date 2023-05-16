f249db38aab352096e1bfbedaae862a1d0655c20
Alexey Mileev <alemileev@gmail.com>
2023-05-16 15:21:55 +0400

    Still figuring out the colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,75 +13,126 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- [[ Name of colorscheme ]]
+-- local colorschemeName = "tokyonight-night"
+-- local colorschemeName = "catppuccin-mocha"
+local colorschemeName = "onedark"
+
 -- [[ Install and configure plugins via lazy.nvim ]]
 require("lazy").setup(
   -- [[ Plugins ]]
-
-  -- Colorscheme
   {
-    "folke/tokyonight.nvim",
-    branch = "main",
-    lazy = false,
-    config = function()
-      -- vim.cmd.colorscheme("tokyonight-night")
-      vim.cmd.colorscheme("tokyonight")
-    end
+    -- Sort of stdlib
+    {
+      "nvim-lua/plenary.nvim",
+      lazy = false,
+    },
+
+    -- tree-sitter
+    {
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+      build = ":TSUpdate",
+      config = function()
+        require("nvim-treesitter.configs").setup {
+          ensure_installed = { "lua" },
+          highlight = {
+            enable = true,
+            use_languagetree = true,
+          },
+          indent = { enable = true },
+        }
+      end,
+    },
+
+    -- TODO: https://github.com/rockerBOO/awesome-neovim#colorscheme
+
+    -- Colorscheme
+    -- TODO: https://github.com/folke/tokyonight.nvim
+    -- {
+    --   "folke/tokyonight.nvim",
+    --   branch = "main",
+    --   lazy = false,
+    --   config = function()
+    --     vim.cmd.colorscheme(colorschemeName)
+    --   end,
+    -- },
+
+    -- TODO: https://github.com/navarasu/onedark.nvim
+    {
+      "navarasu/onedark.nvim",
+      lazy = false,
+      config = function()
+        local onedark = require("onedark")
+        onedark.setup {
+          -- style = "warmer",
+          -- style = "deep",
+          style = "darker",
+        }
+        onedark.load()
+      end,
+    },
+
+    -- TODO: https://github.com/catppuccin/nvim
+    -- {
+    --   "catppuccin/nvim",
+    --   name = "catppuccin",
+    --   lazy = false,
+    --   config = function()
+      --   require("catppuccin").setup({
+      --     flavour = "frappe", -- latte, frappe, macchiato, mocha
+      --     background = {
+      --       light = "latte",
+      --       dark = "mocha",
+      --     },
+      --     transparent_background = false,
+      --     show_end_of_buffer = false,
+      --     term_colors = true,
+      --     dim_inactive = {
+      --       enabled = false,
+      --       shade = "dark",
+      --       percentage = 0.15,
+      --     },
+      --     no_italic = false, -- Force no italic
+      --     no_bold = false, -- Force no bold
+      --     styles = {
+      --       comments = { "italic" },
+      --       conditionals = { "italic" },
+      --       loops = {},
+      --       functions = {},
+      --       keywords = {},
+      --       strings = {},
+      --       variables = {},
+      --       numbers = {},
+      --       booleans = {},
+      --       properties = {},
+      --       types = {},
+      --       operators = {},
+      --     },
+      --     color_overrides = {},
+      --     custom_highlights = {},
+      --     integrations = {
+      --       cmp = true,
+      --       gitsigns = true,
+      --       nvimtree = true,
+      --       telescope = true,
+      --       notify = false,
+      --       mini = false,
+      --       -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      --     },
+      --   })
+    --     vim.cmd.colorscheme(colorschemeName)
+    --   end,
+    -- },
   },
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   lazy = false,
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       flavour = "frappe", -- latte, frappe, macchiato, mocha
-  --       background = {
-  --         light = "latte",
-  --         dark = "mocha",
-  --       },
-  --       transparent_background = false,
-  --       show_end_of_buffer = false,
-  --       term_colors = true,
-  --       dim_inactive = {
-  --         enabled = false,
-  --         shade = "dark",
-  --         percentage = 0.15,
-  --       },
-  --       no_italic = false, -- Force no italic
-  --       no_bold = false, -- Force no bold
-  --       styles = {
-  --         comments = { "italic" },
-  --         conditionals = { "italic" },
-  --         loops = {},
-  --         functions = {},
-  --         keywords = {},
-  --         strings = {},
-  --         variables = {},
-  --         numbers = {},
-  --         booleans = {},
-  --         properties = {},
-  --         types = {},
-  --         operators = {},
-  --       },
-  --       color_overrides = {},
-  --       custom_highlights = {},
-  --       integrations = {
-  --         cmp = true,
-  --         gitsigns = true,
-  --         nvimtree = true,
-  --         telescope = true,
-  --         notify = false,
-  --         mini = false,
-  --         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  --       },
-  --     })
-  --     vim.cmd.colorscheme("catppuccin")
-  --   end,
-  -- },
 
   -- [[ Options ]]
   -- https://github.com/folke/lazy.nvim#%EF%B8%8F-configuration
   {
     defaults = { lazy = true },
+
+    install = { colorscheme = { colorschemeName } },
 
     ui = {
       icons = {
