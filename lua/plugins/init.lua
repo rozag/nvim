b3a783a081908c1091a87d9dceb69d8c3fd4f9db
Alexey Mileev <alemileev@gmail.com>
2023-05-23 11:14:41 +0400

    Further lualine setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -579,14 +579,36 @@ require("lazy").setup(
     -- https://github.com/nvim-lualine/lualine.nvim
     {
       "nvim-lualine/lualine.nvim",
-      opts = {
-        options = {
-          icons_enabled = true,
-          theme = colorschemeName,
-          -- component_separators = "|",
-          -- section_separators = "",
-        },
-      },
+      config = function()
+        require("lualine").setup {
+          options = {
+            icons_enabled = true,
+            theme = colorschemeName,
+            component_separators = "|",
+            section_separators = { left = "", right = "" },
+          },
+          sections = {
+            lualine_a = {
+              { "mode", separator = { left = "" }, right_padding = 2 },
+            },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = {},
+            lualine_y = { "filetype", "progress" },
+            lualine_z = {
+              { 'location', separator = { right = '' }, left_padding = 2 },
+            },
+          },
+          inactive_sections = {
+            lualine_a = { "filename" },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { "location" }
+          },
+        }
+      end,
     },
 
   },
