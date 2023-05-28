a53da0568e1ee438b65a9c88cf39b0630e514e6f
Alexey Mileev <alemileev@gmail.com>
2023-05-28 14:24:50 +0400

    Fill column indicator

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -2,11 +2,11 @@
 vim.api.nvim_create_autocmd(
   "TextYankPost",
   {
+    pattern = "*",
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
     callback = function()
       vim.highlight.on_yank { timeout = 300 }
     end,
-    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-    pattern = "*",
   }
 )
 
@@ -20,3 +20,30 @@ vim.api.nvim_create_autocmd(
     end,
   }
 )
+
+-- Line width limit via colorcolumn
+local fillColumnIndicatorGroup = vim.api.nvim_create_augroup(
+  "FillColumnIndicator",
+  { clear = true }
+)
+local function bindFileTypeToFillColumnIndicatorLimit(type, limit)
+  vim.api.nvim_create_autocmd(
+    "FileType",
+    {
+      pattern = type,
+      group = fillColumnIndicatorGroup,
+      callback = function()
+        vim.opt.colorcolumn = limit
+      end,
+    }
+  )
+end
+bindFileTypeToFillColumnIndicatorLimit("dart", "80")
+bindFileTypeToFillColumnIndicatorLimit("dockerfile", "80")
+bindFileTypeToFillColumnIndicatorLimit("go", "80")
+bindFileTypeToFillColumnIndicatorLimit("java", "140")
+bindFileTypeToFillColumnIndicatorLimit("kotlin", "140")
+bindFileTypeToFillColumnIndicatorLimit("lua", "80")
+bindFileTypeToFillColumnIndicatorLimit("markdown", "80")
+bindFileTypeToFillColumnIndicatorLimit("python", "80")
+bindFileTypeToFillColumnIndicatorLimit("rust", "99")
