260275191e860b8a550f21e62042f01d25e8aa5b
Alexey Mileev <alemileev@gmail.com>
2023-05-28 15:19:56 +0400

    Initial mason setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -623,6 +623,45 @@ require("lazy").setup(
       end,
     },
 
+    -- [[ Sort of a package manager ]]
+    -- https://github.com/williamboman/mason.nvim
+    -- Package list: https://mason-registry.dev/registry/list
+    {
+      "williamboman/mason.nvim",
+      build = ":MasonUpdate",
+      cmd = {
+        "Mason",
+        "MasonInstall",
+        "MasonInstallAll",
+        "MasonUninstall",
+        "MasonUninstallAll",
+        "MasonLog",
+      },
+      config = function()
+        require("mason").setup {
+          PATH = "skip",
+          ui = {
+            icons = {
+              package_pending = " ",
+              package_installed = "󰄳 ",
+              package_uninstalled = " 󰚌",
+            },
+            keymaps = {
+              toggle_server_expand = "<CR>",
+              install_server = "i",
+              update_server = "u",
+              check_server_version = "c",
+              update_all_servers = "U",
+              check_outdated_servers = "C",
+              uninstall_server = "X",
+              cancel_installation = "<C-c>",
+            },
+          },
+          max_concurrent_installers = 10,
+        }
+      end,
+    },
+
   },
 
   -- [[ Options ]]
