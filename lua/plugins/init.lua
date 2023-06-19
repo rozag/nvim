52f6b78451f55217b946ca3cd6d352e579daed31
Alexey Mileev <alemileev@gmail.com>
2023-06-19 21:09:28 +0400

    Adaptive width for nvim-tree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -367,9 +367,8 @@ require("lazy").setup(
             update_root = false,
           },
           view = {
-            adaptive_size = false,
+            adaptive_size = true,
             side = "left",
-            width = 40,
             preserve_window_proportions = true,
           },
           git = {
