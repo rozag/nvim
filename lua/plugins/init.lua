f7702da9b1022e496b6035dafe3843a175e01dee
Alexey Mileev <alemileev@gmail.com>
2023-06-27 20:20:00 +0400

    cmp comparators

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -953,6 +953,32 @@ require("lazy").setup(
             { name = "nvim_lua" },
             { name = "path" },
           },
+          sorting = {
+            comparators = {
+              cmp.config.compare.offset,
+              cmp.config.compare.exact,
+              cmp.config.compare.score,
+              cmp.config.compare.recently_used,
+
+              -- From https://github.com/lukas-reineke/cmp-under-comparator
+              function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find("^_+")
+                local _, entry2_under = entry2.completion_item.label:find("^_+")
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                  return false
+                elseif entry1_under < entry2_under then
+                  return true
+                end
+              end,
+
+              cmp.config.compare.kind,
+              cmp.config.compare.sort_text,
+              cmp.config.compare.length,
+              cmp.config.compare.order,
+            },
+          },
         }
       end,
     },
