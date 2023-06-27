cb67ccd1d39ef65274404b5ca1ea3a597ecba710
Alexey Mileev <alemileev@gmail.com>
2023-06-27 22:52:04 +0400

    Disable lualine in NvimTree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -669,6 +669,7 @@ require("lazy").setup(
             theme = colorschemeName,
             component_separators = "|",
             section_separators = { left = "", right = "" },
+            disabled_filetypes = { "NvimTree" },
           },
           sections = {
             lualine_a = {
