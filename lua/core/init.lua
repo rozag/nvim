b093e80254a63c27ae9ca6e38e841d6ca850c4cb
Alexey Mileev <alemileev@gmail.com>
2023-06-29 22:36:36 +0400

    Moving further with kbd

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,5 +1,6 @@
--- [[ Keybindings ]]
-require("core.kbd")
+-- Set <Space> as the leader key before everything else
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ General options ]]
 require("core.opt")
--- lua/kbd.lua
+++ lua/kbd.lua
@@ -1,14 +1,40 @@
-local nmap = function(keys, func, desc)
-  vim.keymap.set("n", keys, func, { desc = desc })
-end
-
 -- TODO: extract plugin access
 local wk = require("which-key")
+
+local nmap = function(keys, func, desc)
+  vim.keymap.set(
+    "n",
+    keys,
+    func,
+    { desc = desc, noremap = true, silent = true }
+  )
+end
 
 local M = {}
 
 -- [[ General keybindings ]]
 M.general = function()
+  -- Better default experience with <Space> as leader key
+  vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", {
+    desc = "map <Space> to <Nop>",
+    noremap = true,
+    silent = true,
+  })
+
+  -- Remap for dealing with word wrap
+  vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", {
+    desc = "go up in wrapped lines",
+    noremap = true,
+    silent = true,
+    expr = true,
+  })
+  vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", {
+    desc = "go down in wrapped lines",
+    noremap = true,
+    silent = true,
+    expr = true,
+  })
+
   -- Map <Esc> to exit terminal mode
   vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
@@ -25,18 +51,23 @@ M.general = function()
   wk.register { ["<leader>w"] = { name = "+window" } }
   nmap("<leader>ws", vim.cmd.split, "[w]indow [s]plit")
   nmap("<leader>wv", vim.cmd.vsplit, "[w]indow [v]ertical split")
-  nmap("<leader>wk", vim.cmd.close, "[w]indow [k]ill")
+  nmap("<leader>wc", vim.cmd.close, "[w]indow [c]lose")
   nmap("<leader>w=", function()
     vim.cmd("vertical wincmd =")
     vim.cmd("horizontal wincmd =")
   end, "[w]indows [=]equalize in size")
+  nmap("<leader>wh", "<C-w>h", "[w]indow [h]left")
+  nmap("<leader>wk", "<C-w>k", "[w]indow [k]top")
+  nmap("<leader>wl", "<C-w>l", "[w]indow [l]right")
+  nmap("<leader>wj", "<C-w>j", "[w]indow [j]bottom")
+  nmap("<leader>ww", "<C-w>w", "[w]indow [w]cycle")
 
   -- Buffer-related keybindings
   wk.register { ["<leader>b"] = { name = "+buffer" } }
   nmap("<leader>bn", vim.cmd.bnext, "[b]uffer [n]ext")
   nmap("<leader>bp", vim.cmd.bprevious, "[b]uffer [p]revious")
-  nmap("<leader>bn", vim.cmd.enew, "[b]uffer [n]ew")
-  nmap("<leader>bk", vim.cmd.bdelete, "[b]uffer [k]ill")
+  nmap("<leader>bN", vim.cmd.enew, "[b]uffer [N]ew")
+  nmap("<leader>bc", vim.cmd.bdelete, "[b]uffer [c]lose")
   nmap("<leader>bs", vim.cmd.write, "[b]uffer [s]ave")
   nmap("<leader>bS", vim.cmd.wall, "[b]uffer [S]ave all")
 
@@ -48,6 +79,8 @@ M.general = function()
 
   -- TODO: use for tree
   -- - { key: Key1, mods: Option, chars: "^[[A-1" }
+
+  -- TODO: SPC-o-t to open terminal
 
   -- TODO: general keybindings
 end
