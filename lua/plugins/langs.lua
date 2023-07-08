8bc3e90112755b384926839bf0d1b085de8b0154
Alexey Mileev <alemileev@gmail.com>
2023-07-08 21:54:48 +0400

    Make langs provide treesitter grammars

---
+++ lua/plugins/langs.lua
@@ -0,0 +1,30 @@
+local utils = require("utils")
+
+local langs = {
+  lua = {
+    treesitter_grammars = {
+      "lua",
+      "luadoc",
+      "luap",
+      "luau",
+    },
+  },
+
+  go = {
+    treesitter_grammars = {
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+    },
+  },
+}
+
+local M = {}
+
+M.treesitter_grammars = {}
+for _, lang in pairs(langs) do
+  utils.tbl_insert_all(M.treesitter_grammars, lang.treesitter_grammars)
+end
+
+return M
