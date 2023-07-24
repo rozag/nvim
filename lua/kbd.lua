e0d0f070e8e0ecea6865510918256c5fde14b268
Alexey Mileev <alemileev@gmail.com>
2023-07-24 14:41:09 +0400

    Resolve telescope help kbd conflict

--- lua/kbd.lua
+++ lua/kbd.lua
@@ -55,13 +55,13 @@ M.general = function()
   vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
   -- Without leader key
-  nmap("^[[C-Tab", vim.cmd.bnext, "next buffer")           -- C-Tab
+  nmap("^[[C-Tab", vim.cmd.bnext, "next buffer") -- C-Tab
   nmap("^[[C-S-Tab", vim.cmd.bprevious, "previous buffer") -- C-S-Tab
-  nmap("^[[M-s", vim.cmd.write, "save buffer")             -- Cmd-s
-  nmap("^[[M-S-s", vim.cmd.wall, "save all buffers")       -- Cmd-S-s
+  nmap("^[[M-s", vim.cmd.write, "save buffer") -- Cmd-s
+  nmap("^[[M-S-s", vim.cmd.wall, "save all buffers") -- Cmd-S-s
   nmap("<Esc><Esc>", vim.cmd.nohlsearch, "dismiss search highlight")
-  nmap("^[[M-S-z", vim.cmd.redo, "redo")                   -- Cmd-S-z
-  nmap("^[[M-z", vim.cmd.undo, "undo")                     -- Cmd-z
+  nmap("^[[M-S-z", vim.cmd.redo, "redo") -- Cmd-S-z
+  nmap("^[[M-z", vim.cmd.undo, "undo") -- Cmd-z
   vim.keymap.set("i", "^[[M-z", vim.cmd.undo, {
     desc = "undo",
     noremap = true,
@@ -130,7 +130,7 @@ M.plugins = {
     vim.keymap.set("n", "^[[M-/", function()
       return vim.api.nvim_get_vvar("count") == 0
           and "<Plug>(comment_toggle_linewise_current)"
-          or "<Plug>(comment_toggle_linewise_count)"
+        or "<Plug>(comment_toggle_linewise_count)"
     end, { expr = true, desc = "Comment toggle current line" })
     vim.keymap.set(
       "v",
@@ -142,8 +142,8 @@ M.plugins = {
 
   copilot = {
     accept = "<C-Down>", -- C-j
-    next = "<C-Right>",  -- C-l
-    prev = "<C-Left>",   -- C-h
+    next = "<C-Right>", -- C-l
+    prev = "<C-Left>", -- C-h
     dismiss = "<C-]>",
   },
 
@@ -183,22 +183,22 @@ M.plugins = {
   telescope = {
     mappings = function()
       local actions =
-          require("plugins.telescope").require_module.telescope_actions()
+        require("plugins.telescope").require_module.telescope_actions()
       local trouble =
-          require("plugins.trouble").require_module.trouble_telescope()
+        require("plugins.trouble").require_module.trouble_telescope()
       local editing = require("plugins.editing")
       return {
         -- Normal mode
         n = {
           ["^[[A-c"] = actions.delete_buffer,
-          ["^[[A-h"] = editing.cmd_which_key_telescope,
+          ["^[[M-S-h"] = editing.cmd_which_key_telescope,
           ["^[[A-q"] = actions.close,
           ["^[[M-Return"] = trouble.open_with_trouble,
         },
         -- Insert mode
         i = {
           ["^[[A-c"] = actions.delete_buffer,
-          ["^[[A-h"] = editing.cmd_which_key_telescope,
+          ["^[[M-S-h"] = editing.cmd_which_key_telescope,
           ["^[[A-q"] = actions.close,
           ["^[[M-Return"] = trouble.open_with_trouble,
         },
@@ -231,7 +231,7 @@ M.plugins = {
 
     lsp_on_attach = function()
       local builtin =
-          require("plugins.telescope").require_module.telescope_builtin()
+        require("plugins.telescope").require_module.telescope_builtin()
       nmap("gr", builtin.lsp_references, "[r]eferences")
       nmap("gd", builtin.lsp_definitions, "[d]efinition")
       nmap("gD", builtin.lsp_type_definitions, "type [D]efinition")
