868c150c833e3443814a52b93ccccf3842e955ba
Alexey Mileev <alemileev@gmail.com>
2025-08-15 12:32:04 +0400

    Exclude .venv from rg searches

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,7 +31,7 @@ M.filetypes = {
 
 M.cmd = "Telescope"
 M.cmd_file_browser_current =
-  "Telescope file_browser path=%:p:h select_buffer=true"
+"Telescope file_browser path=%:p:h select_buffer=true"
 M.cmd_file_browser_root = "Telescope file_browser"
 
 M.lazy_defs = {
@@ -81,6 +81,8 @@ M.lazy_defs = {
             "!**/.clj-kondo/.cache/**",
             "--glob",
             "!**/.clj-kondo/imports/**",
+            "--glob",
+            "!**/.venv/**",
           },
           prompt_prefix = "   ",
           initial_mode = "insert",
